5522289aa8872244cc14819939c013e7363e4c85
Prasanna Orugonda (EXT-NSB) <prasanna.orugonda.ext@nokia-sbell.com>
2024-05-01 20:18:48 +0530

    new mg

--- shellscript/Conditions.sh
+++ shellscript/Conditions.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 NUMBER=$1
-if[$NUMBER -gt 10] then
-    print "Number $NUMBER is greater"
-else
-    print "Number $NUMBER is lower"
+
+if[ $NUMBER -gt 10 ] then
+        print "Number $NUMBER is greater"
+    else
+        print "Number $NUMBER is lower"
 fi
