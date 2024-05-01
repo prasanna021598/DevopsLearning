efc0444e2c38055617a3bc27e44ec7996e33ae04
Prasanna Orugonda (EXT-NSB) <prasanna.orugonda.ext@nokia-sbell.com>
2024-05-01 19:24:25 +0530

    new mg

--- shellscript/array.sh
+++ shellscript/array.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 MOVIE=("RRR" "SAHO" "Tillu" "Pushpa")
+#size of above array is 3
+#but index starts with 0
+#read $MOVIE(1) #error in this line
+echo "First movie is :$MOVIES[1]"
 
-read $MOVIE(1)
