f810e4937eaea80f776d5de6917e3184562f1b10
Prasanna Orugonda (EXT-NSB) <prasanna.orugonda.ext@nokia-sbell.com>
2024-05-01 19:37:45 +0530

    new mg

--- shellscript/specialvariables.sh
+++ shellscript/specialvariables.sh
@@ -3,3 +3,6 @@
 echo "All variables: $@"
 echo "Number of variables passed: $#"
 echo "Script name: $0"
+echo "Current working directory: $PWD"
+echo "Home dorectory of the current user: $HOME"
+echo "Which user is running the script: $USER"
