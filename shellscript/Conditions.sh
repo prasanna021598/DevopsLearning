9fe35a5d47f590b3692d7d1c45ad61a47b74e575
Prasanna Orugonda (EXT-NSB) <prasanna.orugonda.ext@nokia-sbell.com>
2024-05-01 20:17:33 +0530

    new mg

---
+++ shellscript/Conditions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+NUMBER=$1
+if[$NUMBER -gt 10] then
+    print "Number $NUMBER is greater"
+else
+    print "Number $NUMBER is lower"
+fi
--- shellscript/specialvariables.sh
+++ shellscript/specialvariables.sh
@@ -6,3 +6,7 @@ echo "Script name: $0"
 echo "Current working directory: $PWD"
 echo "Home dorectory of the current user: $HOME"
 echo "Which user is running the script: $USER"
+echo "host name: $HOSTNAME"
+echo "Proccess ID of the current shell script: $$"
+sleep 60 & # to check the proccess id
+echo "Process ID of the last background command: $!"
