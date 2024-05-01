407b314c8b30b20dabb4b6bad9fe8a1a8341b06c
Prasanna Orugonda (EXT-NSB) <prasanna.orugonda.ext@nokia-sbell.com>
2024-05-01 19:25:02 +0530

    new mg

--- shellscript/array.sh
+++ shellscript/array.sh
@@ -4,5 +4,5 @@ MOVIE=("RRR" "SAHO" "Tillu" "Pushpa")
 #size of above array is 3
 #but index starts with 0
 #read $MOVIE(1) #error in this line
-echo "First movie is :$MOVIES[1]"
+echo "First movie is :$MOVIE[1]"
 
