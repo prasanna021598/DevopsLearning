eb2a643ae6ffc98bdb3071d58ffaa32767fee662
Prasanna Orugonda (EXT-NSB) <prasanna.orugonda.ext@nokia-sbell.com>
2024-05-01 19:13:45 +0530

    new mg

--- shellscript/password.sh
+++ shellscript/password.sh
@@ -3,6 +3,6 @@
 echo "please enter username::"
 read USERNAME #Here username is variable"
 echo "Please enter the password::"
-reaD PASSWORD
+read PASSWORD
 
 echo "Username is :$USERNAME, Passowrd is : $PASSWORD"
