cf78e3d4e622ee2145e245cbcf10fe034139c725
Prasanna Orugonda (EXT-NSB) <prasanna.orugonda.ext@nokia-sbell.com>
2024-05-01 20:56:15 +0530

    new mg

--- shellscript/Installation.sh
+++ shellscript/Installation.sh
@@ -4,7 +4,9 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "please run this script with root access"
-    dnf install mysql -y
+    exit 1 # manually exit if error comes
 else
     echo "You are Super user"
 fi
+
+
